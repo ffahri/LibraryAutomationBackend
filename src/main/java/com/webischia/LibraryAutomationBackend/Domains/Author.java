99b4df4b7a902e5c3836e94916e0a293bc8ac510
ffahri <ffahri@users.noreply.github.com>
2018-05-05 22:04:55 +0300

    Author entity added

---
+++ src/main/java/com/webischia/LibraryAutomationBackend/Domains/Author.java
@@ -0,0 +1,10 @@
+package com.webischia.LibraryAutomationBackend.Domains;
+
+import lombok.Data;
+
+@Data
+public class Author {
+    int authorID;
+    String authorName;
+    String authorLastName;
+}
