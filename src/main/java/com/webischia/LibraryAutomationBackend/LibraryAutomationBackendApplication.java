1476e92015866150a9cdc560c7820ea9aaf5d546
ffahri <ffahri@users.noreply.github.com>
2018-05-05 17:50:08 +0300

    Initial Commit

---
+++ src/main/java/com/webischia/LibraryAutomationBackend/LibraryAutomationBackendApplication.java
@@ -0,0 +1,12 @@
+package com.webischia.LibraryAutomationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAutomationBackendApplication {
+//
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAutomationBackendApplication.class, args);
+	}
+}
