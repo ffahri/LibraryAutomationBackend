dc1152009d9c7f8b1de2f3d28337c10fb03d973d
ffahri <ffahri@users.noreply.github.com>
2018-05-05 21:42:26 +0300

    Access Level entity added

---
+++ src/main/java/com/webischia/LibraryAutomationBackend/Domains/AccessLevel.java
@@ -0,0 +1,9 @@
+package com.webischia.LibraryAutomationBackend.Domains;
+
+import java.util.Date;
+
+public class AccessLevel {
+    int accessID;
+    String accessDescription;
+    Date editDate;
+}
