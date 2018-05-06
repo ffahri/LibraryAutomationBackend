459018e5ac2bcd69468c0ff5478f5ef6e0bd6259
ffahri <ffahri@users.noreply.github.com>
2018-05-06 17:37:00 +0300

    Methods added to UserRepository

--- src/main/java/com/webischia/LibraryAutomationBackend/Repository/UserRepository.java
+++ src/main/java/com/webischia/LibraryAutomationBackend/Repository/UserRepository.java
@@ -1,6 +1,33 @@
 package com.webischia.LibraryAutomationBackend.Repository;
 
+import com.webischia.LibraryAutomationBackend.Domains.AccessLevel;
+import com.webischia.LibraryAutomationBackend.Domains.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public AccessLevel getAccessLevel(int id)
+    {
+        return jdbcTemplate.queryForObject("select accessID,accessDescription from ACCESS_LEVEL where accessID ="+id,
+                (rs,rowNum) ->new AccessLevel(rs.getInt("accessID"),rs.getString("accessDescription")));
+    }
+//    int userID;
+//    int accessID;
+//    String firstName;
+//    String lastName;
+//    String mail;
+//    String phone;
+//    String userPassword;
+    public User findByMail(String mail){
+
+        return jdbcTemplate.queryForObject("select userID,accessID,firstName,lastName,mail,phone,userPassword from USERS where mail ="+mail,
+                (rs,rowNum) ->new User(rs.getInt("userID"),rs.getInt("accessID"),rs.getString("firstName"),
+                        rs.getString("lastName"),rs.getString("mail"),rs.getString("phone"),rs.getString("userPassword")));
+
+    }
 }
--- src/main/java/com/webischia/LibraryAutomationBackend/SecurityConfig/ResourceServerConfig.java
+++ src/main/java/com/webischia/LibraryAutomationBackend/SecurityConfig/ResourceServerConfig.java
@@ -38,6 +38,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .and()
                 .httpBasic();
 
+
     }
 
 }
